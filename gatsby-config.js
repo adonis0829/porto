cc3b6b9cdd918e9b34c22ae17e223730bf68f730
kinxtar <jacklin0317@outlook.com>
2023-07-18 10:00:11 -0400

    fatal: removed the image files in static folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Sammy Sato',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+      'Sammy Sato is a software engineer who specializes in building exceptional digital experiences.',
+    siteUrl: 'https://sammysato.com', // No trailing slash allowed!
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Sammy Sato',
+        short_name: 'Sammy Sato',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
